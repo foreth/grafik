2daf04da23c78c5017559425bfa562c6f98cefe1
foreth <metex0@gmail.com>
2016-10-14 00:47:53 +0200

    Missed header file to previous commit...

--- scheduler.h
+++ scheduler.h
@@ -9,6 +9,9 @@ enum positions { REGULAR, ASSISTANT, CO_MANAGER, MANAGER };
 enum times {QUATER, HALF, THREE, FULL};
 enum days {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY};
 
+
+class Shop;
+
 class Employee
 {
 	short id;
@@ -16,14 +19,24 @@ class Employee
 	string last_name;
 	positions position;
 	times time;
-	
+	short working_hours;
+	float time_multiplier;
+	vector<short> free_days;
 
 public:
 
 	Employee(int id, string first_name, string last_name, positions position, times time);
 	short getID();
 	void printToConsole();
+	void setWorkingHours(short working_hours){ this->working_hours = working_hours; }
+	short getWorkingHours(){ return working_hours; }
+	float getTimeMultiplier(){ return time_multiplier; }
+
+	friend class Shop;
 };
+
+
+//####################################################################################################
 
 class Shop
 {
@@ -31,13 +44,17 @@ class Shop
 	short efficiency;						// it's a ghost variable now :) just an idea
 
 public:
+	Shop(){ srand(time(NULL)); }
 	void addEmployee(Employee& employee);
 	void addEmployee(int id, string first_name, string last_name, positions position, times time);
 	void deleteEmployee(Employee& employee);
 	void deleteEmployee(short id);
 	void listEmployees();
 	void makeSchedule(short month, short year);
+	void assignFreeDays(short days_in_month);
 };
+
+//####################################################################################################
 
 class Time
 {
@@ -48,6 +65,8 @@ public:
 	Time(short hour, short minute);
 	short getHour(){ return hour; }
 };
+
+//####################################################################################################
 
 class WorkHour : public Time
 {
@@ -60,26 +79,37 @@ public:
 
 };
 
+//####################################################################################################
+
 class Day
 {
-	//vector<hour> hour_list;
 	days this_day;
 	vector<WorkHour> time;
+	short date;
 
 public:
 	Day(days this_day);
 	days getDay();
 	vector<WorkHour> getTime();
 	string toString();
+	void setDate(short date){ this->date = date; };
+	short getDate(){ return date; };
 };
+
+//####################################################################################################
 
 class Month
 {
 	vector<Day> day_list;
 	short days_in_month;
+	short weekend_days;
 
 public:
 	Month(short month, short year);
 	void printToConsole();
+	short getWorkingHours();
+	vector<Day> getDayList(){ return day_list; }
+	short getWeekendDays(){ return weekend_days; }
+	short getDaysInMonth(){ return days_in_month; }
 };
 
